3b9920cadcb43aceb957ccc0c106ecdabc629554
Greg Cobb <gcobb@vmware.com>
2021-03-03 23:13:03 +0000

    make proxy an h2c app

---
+++ src/example-apps/proxy/go.mod
@@ -0,0 +1,9 @@
+module example-apps/proxy
+
+go 1.16
+
+require (
+	github.com/onsi/ginkgo v1.15.0
+	github.com/onsi/gomega v1.10.5
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110
+)
--- src/example-apps/proxy/main.go
+++ src/example-apps/proxy/main.go
@@ -1,34 +1,28 @@
 package main
 
 import (
-	"example-apps/proxy/handlers"
 	"fmt"
-	"log"
 	"net/http"
-	"os"
-	"strconv"
+
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 )
 
 func main() {
-	systemPortString := os.Getenv("PORT")
-	port, err := strconv.Atoi(systemPortString)
-	if err != nil {
-		log.Fatal("invalid required env var PORT")
+	h2s := &http2.Server{}
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %v, http: %v", r.URL.Path, r.TLS == nil)
+	})
+
+	server := &http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: h2c.NewHandler(handler, h2s),
 	}
-	stats := &handlers.Stats{Latency: []float64{}}
 
-	mux := http.NewServeMux()
-	mux.Handle("/", &handlers.InfoHandler{Port: port})
-	mux.Handle("/dig/", &handlers.DigHandler{})
-	mux.Handle("/digudp/", &handlers.DigUDPHandler{})
-	mux.Handle("/download/", &handlers.DownloadHandler{})
-	mux.Handle("/dumprequest/", &handlers.DumpRequestHandler{})
-	mux.Handle("/echosourceip", &handlers.EchoSourceIPHandler{})
-	mux.Handle("/ping/", &handlers.PingHandler{})
-	mux.Handle("/proxy/", &handlers.ProxyHandler{Stats: stats})
-	mux.Handle("/stats", &handlers.StatsHandler{Stats: stats})
-	mux.Handle("/timed_dig/", &handlers.TimedDigHandler{})
-	mux.Handle("/upload", &handlers.UploadHandler{})
-
-	http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", port), mux)
+	fmt.Printf("Listening [0.0.0.0:8080]...\n")
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
 }
